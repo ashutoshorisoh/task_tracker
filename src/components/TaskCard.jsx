beb5f13a600c39b95bb62cf73e8c50755045c20e
Ashutosh <ashutoshorisoh@gmail.com>
2025-03-08 16:08:11 +0530

    task card ui fixed

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -30,10 +30,10 @@ function TaskCard({ task, onEdit }) {
   return (
     <div 
       id={`task-${task.id}`}
-      className={`bg-white p-4 rounded-lg shadow border-l-4 
-        ${task.priority?.toUpperCase() === 'HIGH' ? 'border-red-500' : 
-          task.priority?.toUpperCase() === 'MEDIUM' ? 'border-yellow-500' : 'border-green-500'} 
-        hover:shadow-md transition-shadow mb-2`}
+      className={` p-4 rounded-xl shadow border-l-4  
+        ${task.priority?.toUpperCase() === 'HIGH' ? 'border-red-500 bg-red-200' : 
+          task.priority?.toUpperCase() === 'MEDIUM' ? 'border-yellow-500 bg-yellow-200' : 'border-green-500  bg-green-200'} 
+         hover:shadow-xl transition-shadow mb-2`}
     >
       <div className="flex items-start">
         <div className="flex-shrink-0 mt-0.5">
@@ -41,7 +41,7 @@ function TaskCard({ task, onEdit }) {
             id={`toggle-${task.id}`}
             onClick={handleToggleStatus}
             className={`h-5 w-5 rounded-full border flex items-center justify-center 
-              ${task.completed ? 'bg-indigo-500 border-indigo-500' : 'border-gray-300'}`}
+              ${task.completed ? 'bg-indigo-500 border-indigo-500' : 'border-gray-700'}`}
           >
             {task.completed && <Check className="h-3 w-3 text-white" />}
           </button>
